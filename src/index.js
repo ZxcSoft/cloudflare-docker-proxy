d691ba2c6b000389346c8b4ee6f9b32324a417a7
hu18650 <hu18650@163.com>
2024-06-09 21:16:05 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.zxcsoft.top": "https://registry-1.docker.io",
+  "quay.zxcsoft.top": "https://quay.io",
+  "gcr.zxcsoft.top": "https://gcr.io",
+  "k8s-gcr.zxcsoft.top": "https://k8s.gcr.io",
+  "k8s.zxcsoft.top": "https://registry.k8s.io",
+  "ghcr.zxcsoft.top": "https://ghcr.io",
+  "cloudsmith.zxcsoft.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
